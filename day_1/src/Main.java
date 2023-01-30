b99c6ee664d20c21b8d57458248129634c1576e2
Roman <avanagi.payment@gmail.com>
2023-01-30 18:42:59 +0300

    Initialize First Sorting

--- day_1/src/Main.java
+++ day_1/src/Main.java
@@ -14,8 +14,21 @@ class Vector{
         base = new int[size];
     }
 
+    Vector(Vector vector){
+        this.size = vector.size;
+        base = new int[size];
+    }
+
     public int get_size(){
         return this.size;
+    }
+
+    public int get_elem(int i){
+        return this.base[i];
+    }
+
+    public void set_elem(int i, int elem){
+        this.base[i] = elem;
     }
 
     public void input(){
@@ -33,11 +46,65 @@ class Vector{
         System.out.println();
     }
 }
+
+
 public class Main {
     public static void main(String[] args) {
         Vector vec = new Vector(3);
         vec.input();
         vec.output();
-        System.out.println(vec.get_size());
+        mergeSort(vec, 0, vec.get_size() - 1);
+        vec.output();
+    }
+
+    public static void quickSort(Vector vector) {
+
+    }
+
+    public static void mergeSort(Vector vector, int left, int right) {
+        if(left >= right) {
+            return;
+        } else {
+            int middle = (right + left)/2;
+            mergeSort(vector, left, middle);
+            mergeSort(vector, middle + 1, right);
+            merge(vector, left, middle, right);
+        }
+    }
+
+    public static void merge(Vector vector, int left, int middle, int right) {
+        int leftLength = middle - left + 1;
+        int rightLength = right - middle;
+
+        int leftVector[] = new int[leftLength];
+        int rightVector[] = new int[rightLength];
+
+        for(int i = 0; i < leftLength; i++){
+            leftVector[i] = vector.get_elem(left + i);
+        }
+        for(int i = 0; i < rightLength; i++){
+            rightVector[i] = vector.get_elem(middle + 1 + i);
+        }
+
+        int leftIndex = 0; int rightIndex = 0;
+
+        for(int i = left; i < right + 1; i++){
+            if(leftIndex < leftLength && rightIndex < rightLength){
+                if (leftVector[leftIndex] < rightVector[rightIndex]) {
+                    vector.set_elem(i, leftVector[leftIndex]);
+                    leftIndex++;
+                }
+                else {
+                    vector.set_elem(i, rightVector[rightIndex]);
+                    rightIndex++;
+                }
+            } else if (leftIndex < leftLength) {
+                vector.set_elem(i, leftVector[leftIndex]);
+                leftIndex++;
+            } else if (rightIndex < rightLength){
+                vector.set_elem(i, rightVector[rightIndex]);
+                rightIndex++;
+            }
+        }
     }
 }
